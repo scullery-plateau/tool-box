7ec77eba04c3f5f21b4438a49ebdf332dc5c010d
Voltron42 <dajohnson1978@gmail.com>
2024-08-23 19:49:27 -0500

    testing fetching and parsing dataset

--- src/tool_box/outfitter_datasets.clj
+++ src/tool_box/outfitter_datasets.clj
@@ -1,4 +1,7 @@
-(ns tool-box.outfitter-datasets)
+(ns tool-box.outfitter-datasets 
+  (:require [clj-http.client :as client]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]))
 
 (def body-types [:fit :hulk :superman :woman])
 
@@ -6,3 +9,5 @@
 
 (def path "https://scullery-plateau.github.io/apps/outfitter/datasets/%s.json")
 
+(defn get-dataset [body-type-index]
+  (client/get (format path (str/join "." (cons (name (nth body-types body-type-index)) version))) {:accept :json}))
--- test/tool_box/outfitter_datasets_test.clj
+++ test/tool_box/outfitter_datasets_test.clj
@@ -1,1 +1,11 @@
-(ns tool-box.outfitter-datasets-test)
+(ns tool-box.outfitter-datasets-test 
+  (:require [clojure.pprint :as pp]
+            [clojure.test :as t]
+            [tool-box.outfitter-datasets :as tbod]
+            [cheshire.core :as json]))
+
+(t/deftest test-get-dataset
+  (t/testing "testing get-dataset"
+    (let [resp (:body (tbod/get-dataset 0))
+          data (json/parse-string resp true)]
+      (pp/pprint (keys (:parts data))))))
