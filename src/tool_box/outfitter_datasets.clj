f1ef49a02459430dcd2eeebf61060da0deefe9e6
Voltron42 <dajohnson1978@gmail.com>
2024-08-24 23:05:26 -0500

    adding constants from outfitter app

--- src/tool_box/outfitter_datasets.clj
+++ src/tool_box/outfitter_datasets.clj
@@ -1,10 +1,61 @@
 (ns tool-box.outfitter-datasets 
   (:require [clj-http.client :as client]
-            [clojure.pprint :as pp]
             [clojure.string :as str]))
 
 (def body-types [:fit :hulk :superman :woman])
 
+(def torso-tops 
+  {:fit 106.35
+   :hulk 169.1
+   :superman 146.9
+   :woman 93.6})
+
+(def head-parts 
+  #{:beard :ears :eyebrows :eyes :hair :hat :head :mask :mouth :nose})
+
+(def part-groups
+  #{"Body"
+    "Face"
+    "Tights"
+    "Clothing"
+    "Back"
+    "Accessories"})
+
+(def part-type-list
+  {:accessories_and_shields ["Accessories" "Accessories & Shields"]
+   :arm "Body"
+   :back "Back"
+   :beard "Face"
+   :belt "Clothing"
+   :boots "Clothing"
+   :chest "Clothing"
+   :collar "Clothing"
+   :ears "Face"
+   :eyebrows "Face"
+   :eyes "Face"
+   :gauntlets "Clothing"
+   :gloves "Tights"
+   :guns "Accessories"
+   :hair "Face"
+   :hat "Clothing"
+   :head "Body"
+   :legs "Body"
+   :mask "Tights"
+   :melee_weapons ["Accessories" "Melee Weapons"]
+   :mouth "Face"
+   :nose "Face"
+   :pants "Clothing"
+   :ranged_weapons ["Accessories" "Ranged Weapons"]
+   :shirt "Tights"
+   :sholders ["Clothing" "Shoulders"]
+   :stockings "Tights"
+   :swords "Accessories"
+   :symbol_A ["Accessories" "Symbol A"]
+   :symbol_B ["Accessories" "Symbol B"]
+   :tights ["Tights" "Leggings"]
+   :torso "Body"
+   :wings_and_tails ["Back" "Wings & Tails"]})
+ 
 (def version [0 0 1])
 
 (def path "https://scullery-plateau.github.io/apps/outfitter/datasets/%s.json")
